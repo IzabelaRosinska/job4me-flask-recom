7a5e475e01b9b9b004a71d1c7d83b336936181a1
Wiktor Jeżowski <260426@student.pwr.edu.pl>
2023-11-02 14:42:37 +0100

    MIWM-254: adding filter option for request without job fair recognition

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from recommendation import Recommender
 from file_reader import *
+from request_maps import *
+from utils import get_dict_part
 
 app = Flask(__name__)
 
@@ -28,11 +30,39 @@ def index():
     return 'Hello!'
 
 
-@app.route('/recommend/<employee_id>')
-def recommend(employee_id):
+@app.route('/recommend/<job_fairs_id>/<employee_id>')
+def recommend(job_fairs_id, employee_id):
+    filter_params = {}
+    if loc := request.args.get('loc'):
+        filter_params['localizations'] = loc.split(';')
+    if forms := request.args.get('forms'):
+        filter_params['forms'] = [forms_map[form] for form in forms.split(';') if form in forms_map]
+    try:
+        if salary := request.args.get('salary'):
+            filter_params['salary'] = int(salary)
+    except ValueError:
+        pass
+    if contract_type := request.args.get('contract_types'):
+        filter_params['contract_types'] = [contract_types_map[c_t] for c_t in contract_type.split(';') if c_t in
+                                           contract_types_map]
+    if levels := request.args.get('levels'):
+        filter_params['levels'] = [levels_map[level] for level in levels.split(';') if level in levels_map]
     response = []
-    employee = employees[employee_id]
-    ranking = recommender.get_offers_ranking(employee, branches_weights)
+    try:
+        employee = employees[employee_id]
+    except Exception:
+        return {'error': 'Wrong id'}
+    ranking = recommender.get_offers_ranking(employee, filter_params,
+                                             get_dict_part(branches_weights, employee['branches']))
     for offer in ranking:
         response.append(offer)
     return jsonify(data=response), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+@app.route('/process/<offer_id>')
+def process(offer_id):
+    if offer_id not in offers_data:
+        return {'error': "Wrong id"}
+    offer = offers_data[offer_id]
+    recommender.load_and_save_offer(offer_id, offer, branches_weights)
+    return "Success"
