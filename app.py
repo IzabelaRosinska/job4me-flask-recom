7e8aba1bcab306af5d4c55b50549b2f4e21f3eac
Wiktor Jeżowski <260426@student.pwr.edu.pl>
2023-11-14 04:10:00 +0100

    MIWM-258: Add services to working commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
 import pyodbc
-from flask import Flask
+from flask import Flask, request, jsonify
 
+from db_connection.db_connect import get_all_offers, get_employee_by_id, get_offer_by_id
 from file_reader import load_labels, read_json
 from recommendation import Recommender
 
@@ -13,13 +14,15 @@ b = '0'
 c = '0'
 d = '0'
 message = ''
-try:
-    username = os.getenv('DB_USERNAME')
-    password = os.getenv('DB_PASSWORD')
-except Exception:
-    username = "miwm"
-    password = "job4meZPI"
-    a = '1'
+# try:
+#     username = os.getenv('DB_USERNAME')
+#     password = os.getenv('DB_PASSWORD')
+# except Exception:
+#     username = "miwm"
+#     password = "job4meZPI"
+#     a = '1'
+username = "miwm"
+password = "job4meZPI"
 server = "tcp:miwmjob4me.database.windows.net,1433"
 database = 'miwm'
 driver = '{ODBC Driver 17 for SQL Server}'
@@ -37,15 +40,16 @@ try:
                                                  ('Ogólne', 'files/labels_soft_skills.json', 1),
                                                  ('Języki', 'files/labels_languages.json', 5)])
 
-    offers = read_json('files/offers.json')
-    offers_embeddings = read_json('files/offers_embeddings.json')
+    # offers = read_json('files/offers.json')
+    # offers_embeddings = read_json('files/offers_embeddings.json')
+
+    offers, offers_embeddings = get_all_offers(cursor)
 except Exception:
     c = '1'
 
 try:
     recommender = Recommender(labels_data)
     recommender.load_offers(offers, branches_weights,
-                            labels=read_json("files/offers_labels.json"),
                             embeddings=offers_embeddings)
 except Exception:
     d = '1'
@@ -54,3 +58,52 @@ except Exception:
 @app.route('/')
 def index():
     return 'Hello! ' + a + b + c + d + ' ' + message
+
+@app.route('/recommend/<job_fairs_id>/<employee_id>', methods=['GET'])
+# @api_key_recommend
+def recommend(job_fairs_id, employee_id: str):
+    filter_params = {}
+    if loc := request.args.get('loc'):
+        filter_params['localizations'] = loc.split(';')
+    if forms := request.args.get('forms'):
+        try:
+            filter_params['forms'] = [int(form) for form in forms.split(';')]
+        except ValueError:
+            pass
+    if salary := request.args.get('salary'):
+        try:
+            filter_params['salary'] = int(salary)
+        except ValueError:
+            pass
+    if contract_type := request.args.get('contract_types'):
+        try:
+            filter_params['contract_types'] = [int(c_t) for c_t in contract_type.split(';')]
+        except ValueError:
+            pass
+    if levels := request.args.get('levels'):
+        try:
+            filter_params['levels'] = [int(level) for level in levels.split(';')]
+        except ValueError:
+            pass
+    response = []
+    try:
+        if not (employee := get_employee_by_id(cursor, int(employee_id))):
+            return {'error': 'Employee not found'}
+    except ValueError:
+        return {'error': 'Wrong id'}
+    ranking = recommender.get_offers_ranking(employee, filter_params, branches_weights)
+    for offer in ranking:
+        response.append(offer)
+    return jsonify(data=response)
+
+
+@app.route('/process/<offer_id>', methods=['GET'])
+# @api_key_process
+def process(offer_id):
+    try:
+        if not (offer := get_offer_by_id(cursor, int(offer_id))):
+            return {'error': 'Offer not found'}
+    except ValueError:
+        return {'error': 'Wrong id'}
+    recommender.load_and_save_offer(offer_id, offer, branches_weights)
+    return "Success"
