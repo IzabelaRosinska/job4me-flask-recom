12efc34df50ac932e07536f6baac282f70196e4d
Wiktor Jeżowski <107326026+BasseChan@users.noreply.github.com>
2023-11-22 13:41:35 +0100

    MIWM-353: Add api authentication (#17)
    * MIWM-353: Add api authentication
    
    * MIWM-353: Add database connection for services
    
    ---------
    
    Co-authored-by: Wiktor Jeżowski <260426@student.pwr.edu.pl>

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 from connection.db_connect import *
 from utils.file_reader import load_labels
 from matching.recommendation import Recommender
+from connection.api_authentication import *
 
 app = Flask(__name__)
 
@@ -31,6 +32,8 @@ recommender = Recommender(labels=labels_data, branches_weights=branches_weights)
 recommender.load_offers(offers, offers_embeddings)
 recommender.load_employees(employees, employees_embeddings)
 
+conn.close()
+
 
 @app.route('/')
 def index():
@@ -38,7 +41,9 @@ def index():
 
 
 @app.route('/recommend/<job_fairs_id>/<employee_id>', methods=['GET'])
+@api_key_recommend
 def recommend(job_fairs_id, employee_id: str):
+
     filter_params = {}
     if loc := request.args.get('loc'):
         filter_params['localizations'] = loc.split(';')
@@ -70,69 +75,102 @@ def recommend(job_fairs_id, employee_id: str):
         job_fairs_id = int(job_fairs_id)
     except ValueError:
         return {'error': 'Wrong job fair id'}
+
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
     employers_on_job_fairs = get_employers_on_job_fairs(cursor, job_fairs_id)
+    conn.close()
+
     response = recommender.get_offers_ranking(employee_id, employers_on_job_fairs, filter_params)
     return jsonify(data=response)
 
 
 @app.route('/update_offer/<offer_id>', methods=['GET'])
+@api_key_update_offer
 def update_offer(offer_id):
     try:
-        if not (offer := get_offer_by_id(cursor, int(offer_id))):
-            return {'error': 'Offer not found'}
+        offer_id = int(offer_id)
     except ValueError:
         return {'error': 'Wrong id'}
-    recommender.update_offer_labels(int(offer_id), offer)
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
+    if not (offer := get_offer_by_id(cursor, offer_id)):
+        return {'error': 'Offer not found'}
+    conn.close()
+    recommender.update_offer_labels(offer_id, offer)
     return "Offer labels updated successfully"
 
 
 @app.route('/update_offers_embeddings', methods=['GET'])
+@api_key_update_offers_embeddings
 def update_offers_embeddings():
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
     embeddings = get_offers_embeddings_only(cursor)
+    conn.close()
     recommender.update_offers_embeddings(embeddings)
     return "Offers embeddings updated successfully"
 
 
 @app.route('/update_employee/<employee_id>', methods=['GET'])
+@api_key_update_employee
 def update_employee(employee_id):
     try:
-        if not (employee := get_offer_by_id(cursor, int(employee_id))):
-            return {'error': 'Employee not found'}
+        employee_id = int(employee_id)
     except ValueError:
         return {'error': 'Wrong id'}
-    recommender.update_employee_labels(int(employee_id), employee)
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
+    if not (employee := get_offer_by_id(cursor, employee_id)):
+        return {'error': 'Employee not found'}
+    conn.close()
+    recommender.update_employee_labels(employee_id, employee)
     return "Employee labels updated successfully"
 
 
 @app.route('/update_employees_embeddings', methods=['GET'])
+@api_key_update_employees_embeddings
 def update_employees_embeddings():
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
     embeddings = get_employees_embeddings_only(cursor)
+    conn.close()
     recommender.update_employees_embeddings(embeddings)
     return "Employees embeddings updated successfully"
 
 
 @app.route('/remove_offer/<offer_id>', methods=['GET'])
+@api_key_remove_offer
 def remove_offer(offer_id):
     try:
         offer_id = int(offer_id)
     except ValueError:
         return {'error': 'Wrong id'}
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
     if check_if_offer_is_disabled(cursor, offer_id):
+        conn.close()
         recommender.remove_offer(offer_id)
         return 'Offer removed from service memory'
     else:
+        conn.close()
         return {'error': 'Offer is still active'}
 
 
 @app.route('/remove_employee/<employee_id>', methods=['GET'])
+@api_key_remove_employee
 def remove_employee(employee_id):
     try:
         employee_id = int(employee_id)
     except ValueError:
         return {'error': 'Wrong id'}
+    conn = pyodbc.connect(f'SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver}')
+    cursor = conn.cursor()
     if check_if_employee_is_deleted(cursor, employee_id):
+        conn.close()
         recommender.remove_employee(employee_id)
         return 'Emloyee removed from service memory'
     else:
+        conn.close()
         return {'error': 'Employee is still in database'}
 
---
+++ connection/api_authentication.py
@@ -0,0 +1,60 @@
+import os
+
+from flask import request, jsonify
+
+
+def verify_api_key():
+    if provided_api_key := request.headers.get('API-Key'):
+        if provided_api_key == os.environ['API_KEY']:
+            return jsonify(data="Wrong API key."), 401
+        return None
+    return jsonify(data="API key not provided."), 401
+
+
+def api_key_recommend(func):
+    def wrapper_recommend(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_recommend
+
+
+def api_key_update_offer(func):
+    def wrapper_update_offer(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_update_offer
+
+
+def api_key_update_employee(func):
+    def wrapper_update_employee(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_update_employee
+
+
+def api_key_update_offers_embeddings(func):
+    def wrapper_update_offers_embeddings(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_update_offers_embeddings
+
+
+def api_key_update_employees_embeddings(func):
+    def wrapper_update_employees_embeddings(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_update_employees_embeddings
+
+
+def api_key_remove_offer(func):
+    def wrapper_remove_offer(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_remove_offer
+
+
+def api_key_remove_employee(func):
+    def wrapper_remove_employee(*args, **kwargs):
+        return error if (error := verify_api_key()) else func(*args, **kwargs)
+
+    return wrapper_remove_employee
